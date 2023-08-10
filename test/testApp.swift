300621bfaf6f257b6fc5d4fb4561db3fb0f097c0
Leah <leahzuniga287@gmail.com>
2023-08-10 13:15:03 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
